a0a81074f428e4d793bb6e9edb11096e0b472f2f
Nguyen Huy Quoc <huyquoc2xx4@gmail.com>
2025-05-22 07:15:46 +0700

    30%

--- server_HotelBooking/src/app.js
+++ server_HotelBooking/src/app.js
@@ -4,16 +4,16 @@ import cors from "cors";
 import morgan from "morgan";
 import mongoose from "mongoose";
 import connectDB from "./config/mongodb.js";
-import router from "./routers/userRouter.js";
+import clerkWebhook from "./controllers/userController.js";
 import AdminRouter from "./routers/adminRouter.js";
-
+import { clerkMiddleware } from '@clerk/express'
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan("tiny"));
-
+app.use(clerkMiddleware());
 connectDB();
 
-app.use("/api/", router);
+app.use("/api/clerk", clerkWebhook);
 app.use("/api/", AdminRouter);
 export const viteNodeApp = app;
--- server_HotelBooking/src/models/userModels.js
+++ server_HotelBooking/src/models/userModels.js
@@ -2,48 +2,54 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
     {
-        user_id: {
-            type: Number,
-            required: true,
-            unique: true,
-        },
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        password_hash: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        full_name: {
-            type: String,
-        },
-        phone_number: {
-            type: String,
-        },
-        address: {
-            type: String,
-        },
-        profile_picture: {
-            type: String,
-        },
-        created_at: {
-            type: Date,
-            default: Date.now,
-        },
-        updated_at: {
-            type: Date,
-            default: Date.now,
-        },
+        // user_id: {
+        //     type: Number,
+        //     required: true,
+        //     unique: true,
+        // },
+        // username: {
+        //     type: String,
+        //     required: true,
+        //     unique: true,
+        // },
+        // password_hash: {
+        //     type: String,
+        //     required: true,
+        // },
+        // email: {
+        //     type: String,
+        //     required: true,
+        //     unique: true,
+        // },
+        // full_name: {
+        //     type: String,
+        // },
+        // phone_number: {
+        //     type: String,
+        // },
+        // address: {
+        //     type: String,
+        // },
+        // profile_picture: {
+        //     type: String,
+        // },
+        // created_at: {
+        //     type: Date,
+        //     default: Date.now,
+        // },
+        // updated_at: {
+        //     type: Date,
+        //     default: Date.now,
+        // },
+        _id: { type: String, required: true },
+        username: { type: String, required: true },
+        email: { type: String, required: true },
+        image: { type: String, required: true },
+        role: { type: String, enum: ["user", "admin"], default: "user" },
+        recentSearchedCities: [{ type: String, required: true }],
     },
     {
-        timestamps: false, 
+        timestamps: false,
         versionKey: false,
     }
 );
